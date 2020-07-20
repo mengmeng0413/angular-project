d3c3bd054a87f8a719841b872b451143db8a2119
zhaomengmeng <zhaomengmengSX@163.com>
2020-07-20 18:07:45 +0800

    feat: 指引蒙层初版

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,17 +7,77 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent implements OnInit{
+  @ViewChild("mask") mask: ElementRef;
+  @ViewChild("maskDesc") maskDesc: ElementRef;
+  @ViewChild("maskNext") maskNext: ElementRef;
   allRoute: Array<any>;
   title: Function;
-  local = ''
+  local = '';
+  allMask = [
+    {
+      id: 'home',
+      desc: '第一步的说明'
+    },{
+      id: 'map',
+      desc: '第二步的说明'
+    },{
+      id: 'routeTest',
+      desc: '第三步的说明'
+    }
+  ];
   constructor(
     private router: Router
   ) { }
 
    ngOnInit(): void {
+     localStorage.setItem('isClick', 'false')
      this.allRoute = this.router.config
      this.getLocal()
+     setTimeout(() => {
+       this.getMask(this.allMask)
+     },500)
    }
+   
+  getMask(params){
+    let mask = this.mask;
+    if(params.length === 0){
+      mask.nativeElement.style.display = 'none';
+      return;
+    }
+    let {id, desc} = params[0];
+    
+    let ele = document.getElementById(id);
+    let offsetWidth = ele.offsetWidth;
+    let offsetHeight = ele.offsetHeight;
+    let offsetLeft = ele.offsetLeft;
+    let offsetTop = ele.offsetTop;
+    
+    console.log('该元素的各个距离', offsetWidth, offsetHeight, offsetLeft, offsetTop)
+    
+    let scrollWidth = document.body.scrollWidth;
+    let scrollHeight =  document.body.scrollHeight;
+    
+    console.log('屏幕大小', scrollWidth, scrollHeight);
+    
+    mask.nativeElement.style.width = scrollWidth + 'px';
+    mask.nativeElement.style.height = scrollHeight + 'px';
+    mask.nativeElement.style.borderColor = "rgba(60, 60, 60, 0.5)";
+    mask.nativeElement.style.borderStyle = 'solid';
+    mask.nativeElement.style.borderLeftWidth = offsetLeft - 5 + 'px';
+    mask.nativeElement.style.borderRightWidth = (scrollWidth - offsetWidth - offsetLeft - 5) + 'px';
+    mask.nativeElement.style.borderTopWidth = offsetTop - 5 + 'px';
+    mask.nativeElement.style.borderBottomWidth = (scrollHeight - offsetHeight - offsetTop - 5) + 'px';
+    mask.nativeElement.style.position = 'absolute';
+    mask.nativeElement.style.left = 0;
+    mask.nativeElement.style.top = 0;
+    let maskDesc = this.maskDesc;
+    maskDesc.nativeElement.innerHTML = desc;
+  }
+  
+  nextClick(){
+    this.allMask.shift();
+    this.getMask(this.allMask);
+  }
   
   routeTo(item: any) {
     this.router.navigateByUrl(item.path).then(r => {
