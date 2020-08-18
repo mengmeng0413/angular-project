dad469f72367670eba27acb90eeea967e261d419
zhaomengmeng <zhaomengmengSX@163.com>
2020-08-18 10:09:45 +0800

    feat: flexLayout练习

--- src/app/flex-layout/flex-layout.component.ts
+++ src/app/flex-layout/flex-layout.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-flex-layout',
   templateUrl: './flex-layout.component.html',
-  styleUrls: ['./flex-layout.component.css']
+  styleUrls: ['./flex-layout.component.less']
 })
 export class FlexLayoutComponent implements OnInit {
 
